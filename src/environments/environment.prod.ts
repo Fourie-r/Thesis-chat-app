28c9614c4ae720f746e5f59ec6b382a5f4cf8b2f
Fourie-r <try.mihaylov@gmail.com>
2018-09-24 12:03:09 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBK24IjrTqfaChH_BRBGYx4tW-EBEeTi_A',
+    authDomain: 'spa-kanban-board-app.firebaseapp.com',
+    databaseURL: 'https://spa-kanban-board-app.firebaseio.com',
+    projectId: 'spa-kanban-board-app',
+    storageBucket: '',
+    messagingSenderId: '665328996357'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBK24IjrTqfaChH_BRBGYx4tW-EBEeTi_A',
+    authDomain: 'spa-kanban-board-app.firebaseapp.com',
+    databaseURL: 'https://spa-kanban-board-app.firebaseio.com',
+    projectId: 'spa-kanban-board-app',
+    storageBucket: '',
+    messagingSenderId: '665328996357'
+  }
+};
